65f22705e858d1455957ca699f6ac39f3cca3fb6
Roberto <robertosarta.rm@gmail.com>
2025-10-08 09:10:01 +0200

    cambio el bcrypt de la contraseña por Hash::make

--- TFM2_api/database/seeders/UserSeeder.php
+++ TFM2_api/database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -17,7 +18,7 @@ class UserSeeder extends Seeder
             'name' => 'Roberto',
             'email' => 'admin@gmail.com',
             'email_verified_at' => now(),
-            'password' => bcrypt('admin123'),
+            'password' => Hash::make('admin123'),
             'telefono' => '608854163',
             'direccion' => 'Calle Ejemplo 123',
             'rol' => 'admin',
